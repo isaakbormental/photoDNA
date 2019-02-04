8ae7be8bb13c5e8fef6900318e3d541126c8762b
Alan <alannetoff@gmail.com>
2019-02-04 15:47:43 +0300

    Add ip to sllowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(s)zltdosr59hq)9!niu)2bu1o=jm$f6@k3dy!(vo9x9&1s1wx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '54.173.181.173']
 
 
 # Application definition
