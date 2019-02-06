26a71da9528972bdefad7c5ff90a104c645b7048
Alan <alannetoff@gmail.com>
2019-02-06 13:31:36 +0300

    Fix mime type

--- photodna/post_fb.py
+++ photodna/post_fb.py
@@ -243,7 +243,7 @@ def do_post_shit(jason):
 
     #'/for_posting/final.png'
     with open('/var/www/html/backend/photoDNA/photodna/for_posting/' + rand_file_name + '.png', 'rb') as data:
-        s3.upload_fileobj(data, 'storage.ws.pho.to', 'photohack/stckrs/' + rand_file_name + '.png')
+        s3.upload_fileobj(data, 'storage.ws.pho.to', 'photohack/stckrs/' + rand_file_name + '.png', ExtraArgs={'ContentType': 'image/png'})
     # potim v amazon + return url
     if os.path.exists('/var/www/html/backend/photoDNA/photodna/for_posting/' + rand_file_name + '.png'):
         os.remove('/var/www/html/backend/photoDNA/photodna/for_posting/' + rand_file_name + '.png')
