9eac2c1b1bb9dd52cbcd65d08c355e29966f6118
Alan <alannetoff@gmail.com>
2019-02-07 11:41:29 +0300

    Latesm morning build

--- photodna/build/precache-manifest.2d5eb55c6aa607e7729eb6f855ab0e68.js
+++ photodna/build/precache-manifest.2d5eb55c6aa607e7729eb6f855ab0e68.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/static/media/icon-chart.764004df.svg"
   },
   {
-    "revision": "653da8c8c45c52ebee0b",
-    "url": "/static/css/main.a384c30d.chunk.css"
+    "revision": "81fd4f0431d7a8136041",
+    "url": "/static/css/main.c11f787f.chunk.css"
   },
   {
     "revision": "8ceaed14016ca7116c77",
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/static/media/icon-nations.05c7b666.svg"
   },
   {
-    "revision": "653da8c8c45c52ebee0b",
-    "url": "/static/js/main.653da8c8.chunk.js"
+    "revision": "81fd4f0431d7a8136041",
+    "url": "/static/js/main.81fd4f04.chunk.js"
   },
   {
     "revision": "23ca16a88131bbfd38f2af30f76bcc0f",
     "url": "/static/media/loading.23ca16a8.svg"
   },
   {
-    "revision": "e8ce2dbad788f0b5239c4a1d82f8cd1e",
-    "url": "/static/media/cat-bg.e8ce2dba.jpg"
-  },
-  {
     "revision": "d81fa03c35e818b01fe34d6e368a8e10",
     "url": "/static/media/male.d81fa03c.svg"
+  },
+  {
+    "revision": "e8ce2dbad788f0b5239c4a1d82f8cd1e",
+    "url": "/static/media/cat-bg.e8ce2dba.jpg"
   },
   {
     "revision": "e9a040b67dc3bf74a017af75a1d8b16d",
     "url": "/static/media/female.e9a040b6.svg"
   },
   {
-    "revision": "2e5f0ece7fd586d3fad2145eed4178a0",
+    "revision": "ec59476fc535ded8ca97a412b11128de",
     "url": "/index.html"
   }
 ];
--- photodna/build/service-worker.js
+++ photodna/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.010989d1145b7b519706c1fabb892b45.js"
+  "/precache-manifest.2d5eb55c6aa607e7729eb6f855ab0e68.js"
 );
 
 workbox.clientsClaim();
