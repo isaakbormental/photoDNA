22441ef090dd9d5880cbffcc6b84c9bd1298de97
Denis Lukashevich <DLukashevich@synergy.ru>
2019-02-07 03:05:25 +0300

    restyle readme page

--- src/pages/PageReadme/PageReadme.js
+++ src/pages/PageReadme/PageReadme.js
@@ -13,18 +13,16 @@ class PageReadme extends Component {
                     switchPage={(page) => this.props.switchPage(page)}
                 />
                 <div className="page_readme__inner">
-                    <div className="page_readme__body">
-                        <h1  className="page_readme__heading">Read more</h1>
-                        <h2 className="page_readme__subheading">Machine learning will help to&nbsp;find more about yourself.</h2>
-                        <p className="page_readme__text">Our technology will compare your features with a&nbsp;database of&nbsp;43&nbsp; nationalities. <br/>Then it&rsquo;ll identify similarities and show which 3&nbsp;nationality you remind the most.</p>
-                        <p className="page_readme__bold-text">Unlike a&nbsp;DNA-test it&rsquo;s free</p>
-                        <p className="page_readme__bold-text">Which nationalities do&nbsp;you look like?</p>
-                    </div>
-                    <Button
-                        className="button page_readme__button"
-                        onClick={() => this.props.chooseImgFromApi()}
-                    >Try now</Button>
+                    <h1  className="page_readme__heading">How we do it</h1>
+                    <h2 className="page_readme__subheading">Machine learning will help to&nbsp;find more about yourself.</h2>
+                    <p className="page_readme__text">Our technology will compare your features with a&nbsp;database of&nbsp;43&nbsp; nationalities. Then it&rsquo;ll identify similarities and show which 3&nbsp;nationality you remind the most.</p>
+                    <p className="page_readme__bold-text">Unlike a&nbsp;DNA-test it&rsquo;s free</p>
+                    <p className="page_readme__bold-text">Which nationalities do&nbsp;you look like?</p>
                 </div>
+                <Button
+                    className="button page_readme__button"
+                    onClick={() => this.props.chooseImgFromApi()}
+                >Try now</Button>
             </div>
         );
 
